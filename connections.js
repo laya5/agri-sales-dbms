5b5077fa7a867079862df7db9ac9426e8aee7996
HARSH-SHETH <ycdtharsh@gmail.com>
2020-10-31 11:01:00 +0530

    Merge conflict resolved.
    .env file removed from index tree.
    connections.js upated.(use process.env.MYSQL_*)
    add ".env" to your local .gitignore file.

--- connections.js
+++ connections.js
@@ -2,18 +2,11 @@ const mysql=require('mysql');
 const express = require("express");
 const result = require('dotenv').config();
 var mysqlConnection =mysql.createConnection({
-<<<<<<< HEAD
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-=======
-    host:"localhost",
-    user:"root",
-    password:"password",
-    database:"agri sales management",
->>>>>>> laya/first
     multipleStatements:true,
     insecureAuth : true
 });
