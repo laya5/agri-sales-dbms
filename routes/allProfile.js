54ea3fee9288ef64415d42d3c58bd9e72454ba6e
Laya Gudala <layagudala@gmail.com>
2021-03-07 15:26:36 +0530

    Delete routes directory

--- routes/allProfile.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const app =express.Router();
-const connection=require("../connections");
-app.get("/main",function(req,res){
-    res.render("userpage");
-})
-module.exports=app;
